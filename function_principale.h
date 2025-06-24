fef321b29ff9b345dc7340794b61e4faa7b7bdf4
HEIL-TCHAMBA-NANA <htchamba124@gmail.com>
2025-06-24 19:59:57 +0100

    Initialisation du projet

---
+++ function_principale.h
@@ -0,0 +1,15 @@
+#ifndef _INCLUDE_FUNCTION_PRINCIPALE_H
+#define _INCLUDE_FUNCTION_PRINCIPALE_H
+
+typedef struct {
+    int user_id;     // Identifiant de l'utilisateur
+    int item_id;     // Identifiant de l'item
+    float rating;    // Note donnée
+    int category_id; // Catégorie (optionnel, selon usage)
+    long timestamp;  // Horodatage
+} Transaction;
+
+
+//Ici on mettra les fonctions qui ne sont pas liees a une bibliotheque particulière comme par exemple les fonctions de manipulation de fichiers.
+
+#endif
