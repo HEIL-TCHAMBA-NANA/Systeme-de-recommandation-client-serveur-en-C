43182776d1834c86001e82152ec60d79fcf19bd5
HEIL-TCHAMBA-NANA <htchamba124@gmail.com>
2025-06-30 20:32:02 +0100

    new commit

--- function_principale.h
+++ function_principale.h
@@ -7,7 +7,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include "struct_transaction.h"
+#include "structures/struct_transaction.h"
 
 //Ici on mettra les fonctions qui ne sont pas liees a une bibliotheque particulière comme par exemple les fonctions de manipulation de fichiers.
 Transaction *load_transactions(const char *filename, int *num_transactions, int *m, int *n);
