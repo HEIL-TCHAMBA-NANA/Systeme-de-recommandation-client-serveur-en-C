3100f8c8615a62932539ff587a101e18980d3bdd
HEIL-TCHAMBA-NANA <htchamba124@gmail.com>
2025-07-01 19:26:24 +0100

    Fact fontionnel

--- structures/struct_transaction.h
+++ structures/struct_transaction.h
@@ -1,6 +1,6 @@
 #ifndef STRUCT_TRANSACTION_H
 #define STRUCT_TRANSACTION_H
-
+#include <math.h>
 typedef struct {
     int user_id;     // Identifiant de l'utilisateur
     int item_id;     // Identifiant de l'item
