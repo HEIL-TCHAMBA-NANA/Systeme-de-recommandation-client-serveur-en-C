20cd8f35c9c30e5a97501b90ca69415940684374
astuce55 <paulwato7@gmail.com>
2025-07-02 03:08:15 +0200

    salut

--- function_principale.h
+++ function_principale.h
@@ -14,14 +14,12 @@
 //Ici on mettra les fonctions qui ne sont pas liees a une bibliotheque particulière comme par exemple les fonctions de manipulation de fichiers.
 Transaction *load_transactions(const char *filename, int *num_transactions, int *m, int *n);
 
-void get_top_n_recommendations(float **full_matrix, int user_id, int m, int n, int N, int *top_items);
-
 long date_to_timestamp(const char *date_str);
 void periodic_transaction(const char *filename, const char *debut_str, const char *fin_str);
 void delete_transaction(const char *filename, int min_U, int min_I);
 void clean_test_file(const char *training, const char *test);
-=========
+
 void get_top_n_recommendations(float **full_matrix, int user_id, int m, int n, int N, int *top_items);
->>>>>>>>> Temporary merge branch 2
+
 
 #endif
